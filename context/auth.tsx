4d121fe39643dd15d7d3db23276967ba2f7cb63b
Pedro Moraes <pm-rogerio@hotmail.com>
2024-10-16 11:28:19 -0300

    Atualização na tela de Login e no Auth para se conectar com a API

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,5 +1,6 @@
 import { router } from "expo-router"
 import { createContext, ReactNode, useContext, useState } from "react"
+import axios from 'axios'
 
 interface IUserLogin {
     email: string
@@ -13,31 +14,38 @@ interface IAuthContext {
 }
 
 interface IAuthProviderProps {
-    children: ReactNode 
+    children: ReactNode
 }
 
 const AuthContext = createContext<IAuthContext>({} as IAuthContext)
 
-export const AuthProvider: React.FC<IAuthProviderProps> = ({children}) => {
-    
-    const [user, setUser] = useState<IUserLogin>({email: '', password: ''})
+export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<IUserLogin>({ email: '', password: '' })
 
-    const handleLogin = () => {
-        if(user.email == 'admin' && user.password == 'admin'){
-            setUser(user)
-            router.push('/home')
-        } else{
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('https://localhost:3000/api/auth/login', { //mudar a url 
+                email: user.email,
+                password: user.password,
+            })
+
+            if (response.status === 200) {
+                setUser(user)
+                router.push('/home')
+            }
+        } catch (error) {
             alert('Email ou senha inválidos!')
         }
     }
-    return ( 
-    <AuthContext.Provider value={{user, setUser, handleLogin}}>
-        {children}
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, handleLogin }}>
+            {children}
         </AuthContext.Provider>
     )
 }
 
-export function useAuth(){
+export function useAuth() {
     const context = useContext(AuthContext)
     return context
 }
